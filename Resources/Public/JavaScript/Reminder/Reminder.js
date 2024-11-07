6ef790d340b0543a519f1132c5f9d6cec122e0db
Sybille Peters <sypets@gmx.de>
2024-11-07 10:52:01 +0100

    Replace top.goToModule() in JavaScript with showModule
    Necessary for TYPO3 v12 support.

--- Resources/Public/JavaScript/Reminder/Reminder.js
+++ Resources/Public/JavaScript/Reminder/Reminder.js
@@ -33,23 +33,7 @@ define(['jquery', 'TYPO3/CMS/Backend/Modal'], function ($, Modal) {
           );
 
           modal.addEventListener('hidden.bs.modal', function() {
-            console.log('pb_notifications: Reminder: in Modal: hidden.bs.modal');
-            /**
-             * @todo go to module user_PbNotificationsNotifications
-             *
-             * TYPO3 v12 top.gotToModule is deprecated in v11 and removed in v12
-             * Deprecation: #94058 - JavaScript goToModule()
-             * https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/11.3/Deprecation-94058-JavaScriptGoToModule.html
-             * Use the following HTML code to replace the inline goToModule() call to for example link to the page module:
-             *
-             * <a href="#"
-             *   data-dispatch-action="TYPO3.ModuleMenu.showModule"
-             *   data-dispatch-args-list="web_layout"
-             *   >
-             *   Go to page module
-             *   </a>
-             */
-             //top.goToModule('user_PbNotificationsNotifications');
+            TYPO3.ModuleMenu.App.showModule('user_PbNotificationsNotifications');
          });
 
         });
